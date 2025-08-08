b66afd470fb350f41774b39e46d1d59b00f2159f
Ishaaq Suffyan <ishaaqsuffyan@gmail.com>
2025-08-08 22:28:27 +0100

    fix: add comma
    i hate json

--- userconfig.js
+++ userconfig.js
@@ -43,7 +43,7 @@ const default_configuration = {
   openLastVisitedTab: true,
   tabs: [
     {
-      name: "life"
+      name: "life",
       background_url: "src/img/banners/banner_14.gif",
       categories: [
         {
